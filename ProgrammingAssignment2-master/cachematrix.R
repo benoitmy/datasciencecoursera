e75c42ce85fa7fb2d6678229833474ed4f09bb1e
=benoitmy <you@example.com>
2020-04-18 19:57:10 +0000

    file for quizz week 3 - course 2

---
+++ ProgrammingAssignment2-master/cachematrix.R
@@ -0,0 +1,36 @@
+## Script containing 2 functions, to calculate inverse of matrix,
+## storing into cache; ## taking inverse of matrix is available in cache.
+
+## Put the inverse matrix in cache
+makeCacheMatrix <- function(x = matrix()) {
+  
+    m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
+
+}
+
+
+## test if matrix is in cache, otherwise calculate
+
+cacheSolve <- function(x, ...) {
+## Return a matrix that is the inverse of 'x'
+  
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
+}
